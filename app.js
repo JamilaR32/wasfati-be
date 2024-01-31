df8dba92af575d824960003885e6a7032a1eee53
Jamila <j.r.cba@hotmail.com>
2024-01-31 18:19:53 +0300

    userfolders+router

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 const connectDB = require("./database");
 const PORT = 8000;
 connectDB();
