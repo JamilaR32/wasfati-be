a1ad310fb7f14c9f90ab0df7227f5ba02da41431
amalal22 <amalalzabi2@gmail.com>
2024-02-01 19:28:00 +0300

    done

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const morgan = require("morgan");
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-const recipeRouter = require("./api/recipe/routers");
+app.use("/media", express.static(path.join(__dirname, "media")));
 
+const recipeRouter = require("./api/recipe/routers");
+//day3
 app.use("/api/recipe", recipeRouter);
 const connectDB = require("./database");
 const PORT = 8000;
