4a26ad0328d9bf94033552819ec389fa542c44d7
loginn <loginn>
2024-02-01 20:37:16 +0300

    doneEditDeleteRecipies

--- api/recipe/routers.js
+++ api/recipe/routers.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const Recipe = require("../../models/Recipe");
+
 const upload = require("../../middlewares/multer");
-const { getAllRecipes, createRecipe } = require("./controller");
+const {
+  getAllRecipes,
+  createRecipe,
+  editRecipe,
+  deleteRecipe,
+} = require("./controller");
 
 router.get("/", getAllRecipes);
 router.post("/", upload.single("image"), createRecipe);
+router.put("/:recipeId", editRecipe);
+router.delete("/:recipeId", deleteRecipe);
 module.exports = router;
