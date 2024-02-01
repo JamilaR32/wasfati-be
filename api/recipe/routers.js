df67e7d9907f14fbe6a8b61f42726bd252f5b4cf
Jamila <j.r.cba@hotmail.com>
2024-02-01 19:45:08 +0300

    fix error

--- api/recipe/routers.js
+++ api/recipe/routers.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Recipe = require("../../models/Recipe");
 const upload = require("../../middlewares/multer");
-const {
-  getAllRecipes,
-  createRecipe,
-  createIngrediant,
-} = require("./controller");
+const { getAllRecipes, createRecipe } = require("./controller");
+const { createIngrediant } = require("../ingrediants/controller");
 
 router.get("/", getAllRecipes);
 router.post("/", upload.single("image"), createRecipe);
--- app.js
+++ app.js
@@ -18,13 +18,12 @@ app.use(passport.initialize());
 passport.use("local", localStrategy);
 app.use("/media", express.static(path.join(__dirname, "media")));
 app.use("/api/recipe", recipeRouter);
-app.use(userRouter);
+app.use("/users", userRouter);
+app.use("/ingrediants", ingrediantRouter);
 
 const PORT = 8000;
 connectDB();
 
-app.use("/users", userRouter);
-app.use("/ingrediants", ingrediantRouter);
 app.use((req, res, next) => {
   res.status(404).json({ message: "Path not found" });
 });
