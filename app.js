464e4257722994e410439000c92b85a3c6472780
amalal22 <amalalzabi2@gmail.com>
2024-02-01 18:13:36 +0300

    done R

--- app.js
+++ app.js
@@ -2,25 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const userRouter = require("./api/user/routers");
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+const recipeRouter = require("./api/recipe/routers");
+
+app.use("/api/recipe", recipeRouter);
 const connectDB = require("./database");
 const PORT = 8000;
 connectDB();
-
-app.use("/users", userRouter);
-
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Path not found" });
-});
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message || "Internal Server Error",
-  });
-});
 
 app.listen(PORT, () => {
   console.log("The application is running on localhost:8000");
